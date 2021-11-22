f9097c585e11ffc1269eaa17b883dfdc1495375b
Sainath Ebitwar <48389381+SEbitwar@users.noreply.github.com>
2021-11-22 10:45:43 +0530

    initial commit
    barebone spring boot project with required dependencies

---
+++ src/main/java/com/goudscode/SpringBootMongodbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.goudscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goudscode/SpringBootMongodbPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goudscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
